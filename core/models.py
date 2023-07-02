9ecbac90d9e57ab14de6a5cb9c267b29afbad464
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2023-07-02 09:27:26 +0530

    adding celery

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from ipynbs.models import TestMode
 # Create your models here.
 
+
 class Person(models.Model):
     name = models.CharField(max_length=50)
 
@@ -22,10 +23,12 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     authors = models.ManyToManyField('Author')
 
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-# Queries 
+
+# Queries
 
 # a1 = Author.objects.create(name="a1")
 
@@ -42,3 +45,42 @@ class Author(models.Model):
 # a1.book_set.all()
 
 # b1.authors.all()
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    population = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    population_factor = models.FloatField(default=1)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    population_factor = models.FloatField(default=1)
+
+    def __str__(self):
+        return self.name
+
+# from django.db import connection
+
+
+# query = 'SELECT * FROM core_country;'
+ 
+# with connection.cursor() as cursor:
+#     res = cursor.execute(query)
+#     columns = [col[0] for col in res.description]
+#     rows = res.fetchall()
+#     result = [dict(zip(columns, row)) for row in rows]
+#     print(result)
+
